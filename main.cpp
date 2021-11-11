035fec6b2e7efd236329ac1d9364cae7cc1858c1
Kabir Samsi <samsikabir@gmail.com>
2021-11-10 17:47:48 -0800

    Commented initial morse tree code

--- main.cpp
+++ main.cpp
@@ -3,43 +3,59 @@
 #include <string>
 #include "binary_tree.hpp"
 
+/**
+  @Purpose - Unscramble a morse code sequence into an English string
+  @param morseTree - The starting node of the morse code binary tree
+  @param morse - The morse code sequence
+  @return - The unscrambled string
+  @author - Kabir Samsi
+**/
+
 std::string unscramble(Node* morseTree, std::string morse) {
-    Node* current = morseTree;
+    Node* current = morseTree; //Track current node
     std::string result = "";
     std::cout << "Original: " << morse << std::endl << "Unscrambled: ";
 
-    for (int i = 0; i <= morse.length(); i++) {
+    for (int i = 0; i <= morse.length(); i++) { //Iterate through each element
         switch(morse[i]) {
-            case '.':
+            case '.': //Short -> traverse left
                 if (current->left != nullptr) current = current->left;
                 break;
 
-            case '-':
+            case '-': //Long -> traverse right
                 if (current->right != nullptr) current = current->right;
                 break;
 
-            default:
-                if (current != nullptr && current->val != ' ') {
+            default: //If space is encountered
+                if (current != nullptr && current->val != ' ') { //If value is not an empty node, add to result string
                     result += current->val;
-                    if (morse[i] < morse.length() && morse[i+1] == ' ') result += ' ';
+                    if (morse[i] < morse.length() && morse[i+1] == ' ') result += ' '; //Add space if double-space encountered
                 }
-                current = morseTree;
+                current = morseTree; //Reset current node to the root node
                 break;
         }
     }
     return result;
 }
 
+/**
+  @Purpose - Execute main method
+  @return - 0
+  @author - Kabir Samsi
+**/
+
 int main() {
-    Node* morseTree = new Node;
+    Node* morseTree = new Node; //Declare root of morse binary tree
     std::string line;
     std::ifstream file;
     
+    //Open cipher key and build binary tree
     morseTree->val = ' ';
     file.open("cipher.txt");
     while (getline(file, line)) build(morseTree, line[0], line);
     file.close();
 
+    //Open input file and translate
     file.open("input.txt");
     while (getline(file, line)) std::cout << unscramble(morseTree, line);
     std::cout << std::endl;
